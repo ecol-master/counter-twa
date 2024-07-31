511fbdd76215d406ae62fd595632df6cb5efca66
Dmitry Kuzmin <dskuzmin@edu.hse.ru>
2024-07-31 13:44:56 +0500

    test commit

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 const manifestUrl =
   "https://ecol-master.github.io/counter-twa/tonconnect-manifest.json";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
