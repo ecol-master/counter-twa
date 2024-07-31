89259e0233177a31c0b2f6da93c71437b7dbf35a
Dmitry Kuzmin <dskuzmin@edu.hse.ru>
2024-07-31 13:19:17 +0500

    fix manifestUrl

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,8 @@ import App from "./App.tsx";
 import "./index.css";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-//const manifestUrl =
-//("https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json");
 const manifestUrl =
-  "https://github.com/ecol-master/counter-twa/tonconnect-manifest.json";
-
+  "https://ecol-master.github.io/counter-twa/tonconnect-manifest.json";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
